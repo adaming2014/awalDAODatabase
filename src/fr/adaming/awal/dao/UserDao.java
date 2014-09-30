d27ed80740d8ef1623cdc50149d44fb83330ec1f
bianloic <bian.loic@gmail.com>
2014-09-30 14:10:10 +0200

    change UserDao to use a Dao Generiq class

--- src/fr/adaming/awal/dao/UserDao.java
+++ src/fr/adaming/awal/dao/UserDao.java
@@ -5,6 +5,7 @@
  */
 package fr.adaming.awal.dao;
 
+import fr.adaming.awal.dao.generiq.Dao;
 import fr.adaming.awal.dao.hibernate.HibernateUtil;
 import fr.adaming.awal.dao.interfaces.IUserDao;
 import fr.adaming.awal.entity.User;
@@ -17,166 +18,11 @@ import org.hibernate.Transaction;
  *
  * @author INTI0219
  */
-public class UserDao implements IUserDao{
+public class UserDao extends Dao<User>{
 
-    Transaction transaction = null;
-
-    public UserDao() 
-    {
-        
-    }
-    
-    @Override
-    public List<User> getAll() {
-        Session session = HibernateUtil.currentSession();
-        
-        session = HibernateUtil.currentSession();
-        List<User> users =  new ArrayList<User>();
-        try 
-        {
-            users = session.createQuery("FROM User").list();
-    
-            transaction.commit();
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-            if(null != transaction && transaction.isActive())
-            {
-                transaction.rollback();
-            }
-        }    
-        HibernateUtil.closeSession();
-        return users;
+    public UserDao(Class<User> entityClass) {
+        super(entityClass);
     }
 
-    @Override
-    public boolean create(User user) {
-        
-        Session session = HibernateUtil.currentSession();
-        boolean create_ok = false;
-        
-        try 
-        {
-            transaction = session.beginTransaction();
-            session.save(user);
-            transaction.commit();
-            create_ok = true;
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-            if(null != transaction && transaction.isActive())
-            {
-                transaction.rollback();
-            }
-        }
-        finally{
-            
-        HibernateUtil.closeSession();
-        return create_ok;
-        }
-    }
-
-    @Override
-    public boolean update(User user) {
-        Session session = HibernateUtil.currentSession();
-        boolean update_ok = false;
-
-        try 
-        {
-            transaction = session.beginTransaction();
-            session.saveOrUpdate(user);
-            transaction.commit();
-            update_ok = true;
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-            if(null != transaction && transaction.isActive())
-            {
-                transaction.rollback();
-            }
-        }
-        HibernateUtil.closeSession(); 
-        return update_ok;
-    }
-
-    @Override
-    public boolean delete(User user) {
-        Session session = HibernateUtil.currentSession();
-        boolean deleteByUser_ok = false;
-        
-        try 
-        {
-            transaction = session.beginTransaction();
-
-            session.delete(user);
-            transaction.commit();
-            deleteByUser_ok = true;
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-            if(null != transaction && transaction.isActive())
-            {
-                transaction.rollback();
-            }
-        }
-        HibernateUtil.closeSession();
-        return deleteByUser_ok;
-    }    
-
-    @Override
-    public boolean delete(int id) {
-        
-        Session session = HibernateUtil.currentSession();
-        boolean deleteById_ok = false;
-
-        try 
-        {
-            transaction = session.beginTransaction();
-            String requete = "FROM User WHERE id_user = :id";
-            User user = (User)session.createQuery(requete).setParameter("id", id).uniqueResult();
-            session.delete(user);
-            transaction.commit();
-            deleteById_ok = false;
-
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-            if(null != transaction && transaction.isActive())
-            {
-                transaction.rollback();
-            }
-        }
-        HibernateUtil.closeSession();
-        return deleteById_ok;
-    }    
-
-    @Override
-    public User getById(int id) {
-        
-        Session session = HibernateUtil.currentSession();
-        User user = new User();
-        
-        try 
-        {
-            transaction = session.beginTransaction();
-            String requete = "FROM User WHERE id_user = :id";
-            user = (User)session.createQuery(requete).setParameter("id", id).uniqueResult();
-        } 
-        catch (Exception e) 
-        {
-            e.printStackTrace();
-            if(null != transaction && transaction.isActive())
-            {
-                transaction.rollback();
-            }
-        }    
-        HibernateUtil.closeSession(); 
-        return user;
-    }
     
 }
