43874ec330729a16282542c4557d0d0558c709a0
bianloic <bian.loic@gmail.com>
2014-09-30 14:07:54 +0200

    add AddressDao class

---
+++ src/fr/adaming/awal/dao/AddressDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.dao;
+
+import fr.adaming.awal.dao.generiq.Dao;
+import fr.adaming.awal.entity.Address;
+
+/**
+ *
+ * @author INTI0217
+ */
+public class AddressDao extends Dao<Address>{
+
+    public AddressDao(Class<Address> entityClass) {
+        super(entityClass);
+    }
+    
+}
