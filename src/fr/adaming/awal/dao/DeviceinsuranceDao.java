845f0b75a5d9c3e5b6b9dd4d0b3d9cd33555b7b7
waelzamouri <waelzamouri@gmail.com>
2014-09-30 11:25:45 +0200

    DeviceinsuranceDao

---
+++ src/fr/adaming/awal/dao/DeviceinsuranceDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.dao;
+
+import fr.adaming.awal.dao.generiq.Dao;
+import fr.adaming.awal.entity.Deviceinsurance;
+
+/**
+ *
+ * @author INTI0227
+ */
+public class DeviceinsuranceDao extends Dao<Deviceinsurance>{
+
+    public DeviceinsuranceDao() {
+        super(Deviceinsurance.class);
+    }
+    
+}
