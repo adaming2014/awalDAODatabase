27b498913a1038576f5b8e26a593ce90fe74d2eb
waelzamouri <waelzamouri@gmail.com>
2014-09-30 11:30:08 +0200

    DevicerepairDao

---
+++ src/fr/adaming/awal/dao/Devicerepair.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.dao;
+
+import fr.adaming.awal.dao.generiq.Dao;
+
+/**
+ *
+ * @author INTI0227
+ */
+public class Devicerepair extends Dao<Devicerepair>{
+
+    public Devicerepair() {
+        super(Devicerepair.class);
+    }
+    
+}
