7187745fb18cec865464cbee1061615fa2f4a7d5
waelzamouri <waelzamouri@gmail.com>
2014-09-30 11:18:44 +0200

    DeviceDao

---
+++ src/fr/adaming/awal/dao/DeviceDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.dao;
+
+import fr.adaming.awal.dao.generiq.Dao;
+import fr.adaming.awal.entity.Device;
+
+/**
+ *
+ * @author INTI0227
+ */
+public class DeviceDao extends Dao<Device>{
+
+    public DeviceDao() {
+        super(Device.class);
+    }
+    
+}
