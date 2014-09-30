c128cae9883e0bd8830d4dc0bd33e791d98aef89
Antoine <git.alucas@gmail.com>
2014-09-30 09:12:32 +0200

    Initial commit

---
+++ src/fr/adaming/awal/dao/RepairerDao.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.adaming.awal.dao;
+
+import fr.adaming.awal.dao.interfaces.IRepairerDao;
+import fr.adaming.awal.entity.Repairer;
+import java.util.List;
+
+/**
+ *
+ * @author INTI0221
+ */
+public class RepairerDao implements IRepairerDao {
+
+    @Override
+    public List<Repairer> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean create(Repairer address) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Repairer address) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(Repairer address) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Repairer getById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
