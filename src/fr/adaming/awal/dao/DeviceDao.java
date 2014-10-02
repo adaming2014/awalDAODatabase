1e52688966e95054333e4079c8471acbf46e5c72
bianloic <bian.loic@gmail.com>
2014-10-02 10:06:37 +0200

    add methode to DeviceDao

--- src/fr/adaming/awal/dao/DeviceDao.java
+++ src/fr/adaming/awal/dao/DeviceDao.java
@@ -6,8 +6,15 @@
 package fr.adaming.awal.dao;
 
 import fr.adaming.awal.dao.generiq.Dao;
+import fr.adaming.awal.dao.hibernate.HibernateUtil;
 import fr.adaming.awal.dao.interfaces.IDeviceDao;
+import fr.adaming.awal.entity.Client;
 import fr.adaming.awal.entity.Device;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -15,8 +22,28 @@ import fr.adaming.awal.entity.Device;
  */
 public class DeviceDao extends Dao<Device> implements IDeviceDao {
 
+    private String reqDevicesByClient = "SELECT u FROM Device u WHERE u.client = :client";
     public DeviceDao() {
         super(Device.class);
+    }
+
+    @Override
+    public List<Device> getDevicesByClient(Client client) {
+        List<Device> entites = null;
+        Session session = HibernateUtil.currentSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Query q = session.createQuery(reqDevicesByClient).setParameter("client", client);
+            entites = q.list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return entites;
     }
 
 }
