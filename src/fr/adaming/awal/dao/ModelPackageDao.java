b5fd1669320aae6f1ae66846a5f3b86cb805c9b7
bianloic <bian.loic@gmail.com>
2014-10-04 10:31:12 +0200

    add method getPackagesByModel in class ModelPackage

--- src/fr/adaming/awal/dao/ModelPackageDao.java
+++ src/fr/adaming/awal/dao/ModelPackageDao.java
@@ -6,8 +6,13 @@
 package fr.adaming.awal.dao;
 
 import fr.adaming.awal.dao.generiq.Dao;
+import fr.adaming.awal.dao.hibernate.HibernateUtil;
 import fr.adaming.awal.dao.interfaces.IModelPackageDao;
+import fr.adaming.awal.entity.Modele;
 import fr.adaming.awal.entity.Modelpackage;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 /**
  *
@@ -15,8 +20,20 @@ import fr.adaming.awal.entity.Modelpackage;
  */
 public class ModelPackageDao extends Dao<Modelpackage> implements IModelPackageDao {
 
+    private String reqPackageByModel = "SELECT u FROM Modelpackage u WHERE u.modele = :modele";
+
     public ModelPackageDao() {
         super(Modelpackage.class);
+    }
+
+    @Override
+    public List<Modelpackage> getPackagesByModel(Modele model) {
+        List<Modelpackage> modelpackages = null;
+        Session session = HibernateUtil.currentSession();
+        Query q = session.createQuery(reqPackageByModel).setParameter("modele", model);
+        modelpackages = q.list();
+        HibernateUtil.closeSession();
+        return modelpackages;
     }
 
 }
