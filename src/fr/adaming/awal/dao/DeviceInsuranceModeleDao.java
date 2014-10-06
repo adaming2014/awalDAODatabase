fcd7af533a12c0139ae7ac4d7f545267ee5c09d2
bianloic <bian.loic@gmail.com>
2014-10-06 15:48:37 +0200

    add method deviceinsuranceByModele

--- src/fr/adaming/awal/dao/DeviceInsuranceModeleDao.java
+++ src/fr/adaming/awal/dao/DeviceInsuranceModeleDao.java
@@ -6,18 +6,35 @@
 package fr.adaming.awal.dao;
 
 import fr.adaming.awal.dao.generiq.Dao;
+import fr.adaming.awal.dao.hibernate.HibernateUtil;
 import fr.adaming.awal.dao.interfaces.IDeviceInsuranceModeleDao;
 import fr.adaming.awal.entity.Deviceinsurancemodel;
+import fr.adaming.awal.entity.Modele;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 /**
  *
  * @author INTI0227
  */
-public class DeviceInsuranceModeleDao extends Dao<Deviceinsurancemodel> 
-implements IDeviceInsuranceModeleDao{
-    
-    public DeviceInsuranceModeleDao(){
+public class DeviceInsuranceModeleDao extends Dao<Deviceinsurancemodel>
+        implements IDeviceInsuranceModeleDao {
+
+    private String reqDeviceInsuranceByModel = "SELECT d FROM Deviceinsurancemodel d WHERE d.modele = :modele";
+
+    public DeviceInsuranceModeleDao() {
         super(Deviceinsurancemodel.class);
     }
-    
+
+    @Override
+    public List<Deviceinsurancemodel> getDeviceInsuranceModelsByModel(Modele model) {
+        List<Deviceinsurancemodel> deviceinsurancemodels = null;
+        Session session = HibernateUtil.currentSession();
+        Query q = session.createQuery(reqDeviceInsuranceByModel).setParameter("modele", model);
+        deviceinsurancemodels = q.list();
+        HibernateUtil.closeSession();
+        return deviceinsurancemodels;
+    }
+
 }
