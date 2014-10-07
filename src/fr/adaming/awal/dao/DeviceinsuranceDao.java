d97c101ef46deb4f5447e672c421e39f3fec04ae
bianloic <bian.loic@gmail.com>
2014-10-07 16:34:27 +0200

    change parameter of method

--- src/fr/adaming/awal/dao/DeviceinsuranceDao.java
+++ src/fr/adaming/awal/dao/DeviceinsuranceDao.java
@@ -8,9 +8,11 @@ package fr.adaming.awal.dao;
 import fr.adaming.awal.dao.generiq.Dao;
 import fr.adaming.awal.dao.hibernate.HibernateUtil;
 import fr.adaming.awal.dao.interfaces.IDeviceInsuranceDao;
+import fr.adaming.awal.entity.Client;
 import fr.adaming.awal.entity.Device;
 import fr.adaming.awal.entity.Deviceinsurance;
 import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
@@ -20,15 +22,19 @@ import org.hibernate.criterion.Restrictions;
  */
 public class DeviceinsuranceDao extends Dao<Deviceinsurance> implements IDeviceInsuranceDao {
 
+    private String reqDevicesInsuranceByClient = "SELECT d FROM Deviceinsurance as c"
+            + " inner join c.device as u WHERE u.client = :client";
+
     public DeviceinsuranceDao() {
         super(Deviceinsurance.class);
     }
 
     @Override
-    public List<Deviceinsurance> getDevicesInsuranceByClient(Device device) {
+    public List<Deviceinsurance> getDevicesInsuranceByClient(Client client) {
         List<Deviceinsurance> entites = null;
         Session session = HibernateUtil.currentSession();
-        entites = session.createCriteria(Deviceinsurance.class).add(Restrictions.eq("device", device)).list();
+        Query q = session.createQuery(reqDevicesInsuranceByClient).setParameter("client", client);
+        entites = q.list();
         HibernateUtil.closeSession();
         return entites;
     }
